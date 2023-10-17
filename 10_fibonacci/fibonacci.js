45e26ef0e2d02e9113243cefa2b87ad7c3fb0eb3
Myleshen Praburam <mylesuge@gmail.com>
2023-10-17 18:20:42 +0530

    Final Commit

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    if(value < 0) {
+        return "OOPS";
+    }
+    let intValue;
+    if(typeof value != Number && typeof value == String) {
+        intValue = parseInt(value);
+    } else {
+        intValue = value;
+    }
+
+    let a = 1;
+    let b = 1;
+
+    for(let i=3; i <= value; i++) {
+        let c = b + a;
+        a = b;
+        b = c;
+    }
+
+    return b;
 
 };
 
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(objectArray) {
+    let titleArray = [];
+    for(let i in objectArray) {
+        titleArray.push(objectArray[i].title);
+    }
+    return titleArray;   
 };
 
 // Do not edit below this line
